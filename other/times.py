c9e0e6baedca56bf2265d4b3707bc83d08b8d6e9
houdinis <houdinis@163.com>
2018-08-09 01:18:01 +0800

    other

---
+++ other/times.py
@@ -0,0 +1,12 @@
+#判断一个数是否为回数
+
+import random
+
+number = input('请输入一个数:')
+
+for i in range(int(len(number))//2):
+    if number[i] != number[-i-1]:
+        print('不是回数!')
+        break
+else:
+    print('这是一个回数!')
